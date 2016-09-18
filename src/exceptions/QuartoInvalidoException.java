6bbabe3071f5acdeba95513d95301d59441b08bd
Dayvson <dayvson.nascimento@ccc.ufcg.edu.br>
2016-09-18 14:35:15 -0300

    Exception nas classes estadia e quarto

--- src/exceptions/QuartoInvalidoException.java
+++ src/exceptions/QuartoInvalidoException.java
@@ -5,7 +5,7 @@ public class QuartoInvalidoException extends Exception {
 	private static final long serialVersionUID = 1L;
 
 	public QuartoInvalidoException() {
-		super("Quarto invadlido");
+		super("Quarto invalido");
 	}
 
 	public QuartoInvalidoException(String msg) {
