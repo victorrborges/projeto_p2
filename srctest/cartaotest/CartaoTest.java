05548ea15e05d604a6203b93adf235418e775d36
wesley.anibal@ccc.ufcg.edu.br <wesley.anibal@ccc.ufcg.edu.br>
2016-10-10 15:12:33 -0300

    umas edições no teste do cartão

--- srctest/cartaotest/CartaoTest.java
+++ srctest/cartaotest/CartaoTest.java
@@ -20,18 +20,21 @@ public class CartaoTest {
 		assertEquals(0, cartao.getPontos());
 		cartao.addPontos(100);
 		assertEquals(10, cartao.getPontos());
-		cartao.convertePontos(10);
+		assertEquals("R$1,00", cartao.convertePontos(10));
 		assertEquals(0, cartao.getPontos());
 	
 	}
 	@Test
 	public void testStrategy() throws Exception{
 		assertTrue(cartao.getTipoDeCartao() instanceof Padrao);
-		cartao.addPontos(3500);
+		cartao.addPontos(4000);
+		assertEquals("R$11,10", cartao.convertePontos(35));
 		cartao.verificaTipo();
 		assertTrue(cartao.getTipoDeCartao() instanceof Premium);
+		assertEquals("R$11,10", cartao.convertePontos(35));
 		cartao.addPontos(8500);
 		cartao.verificaTipo();
 		assertTrue(cartao.getTipoDeCartao() instanceof Vip);
+		assertEquals("R$26,00", cartao.convertePontos(35));
 	}
 }
