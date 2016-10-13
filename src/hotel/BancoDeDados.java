1e7cd69d2297b8f82c286b870ef7762f24017922
WESLEY GONCALVES ANIBAL 115210629 <wesleyga@lcc2-24.lcc.ufcg.edu.br>
2016-10-13 13:48:32 -0300

    passo 9 completo,parte de persisencia feita

--- src/hotel/BancoDeDados.java
+++ src/hotel/BancoDeDados.java
@@ -3,6 +3,7 @@ package hotel;
 import java.io.EOFException;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
@@ -15,17 +16,18 @@ public class BancoDeDados {
 	
 	}
 	public HotelController iniciaSistema() throws IOException, ClassNotFoundException{
-		File file = new File("arquivos_sistema/hug.dat");
-		if(!file.exists()){
-			FileOutputStream fos = new FileOutputStream("arquivos_sistema/hug.dat");
-			fos.close();
+		try {
+			FileInputStream fis = new FileInputStream("arquivos_sistema/hug.dat");
+			GZIPInputStream giz = new GZIPInputStream(fis);
+			ObjectInputStream ois = new ObjectInputStream(giz);
+			HotelController saida = (HotelController) ois.readObject();
+			ois.close();
+			return saida;
+		} catch (FileNotFoundException e) {
+			return new HotelController();
 		}
-		FileInputStream fis = new FileInputStream("arquivos_sistema/hug.dat");
-		GZIPInputStream giz = new GZIPInputStream(fis);
-		ObjectInputStream ois = new ObjectInputStream(giz);
-		HotelController saida = (HotelController) ois.readObject();
-		ois.close();
-		return saida;
+	
+
 	}
 	public void fechasistema(HotelController hotel) throws IOException{
 		
